4f29c434e201cd4d507731fb0e8ebc9fa94f943f
baoson2211 <baoson92@gmail.com>
2015-11-30 19:18:25 +0700

    Commit: no change

--- user/adc.h
+++ user/adc.h
@@ -1,22 +1,14 @@
 /**
   ******************************************************************************
-  * @file    	RTC/Calendar/main.h
-  * @author  	ARMVN Application Team
+  * @file    	../user/adc.c
+  * @author  	Bao Son Le - ET02 - K55
   * @version 	V1.0.0
-  * @date    	01/30/2010
-  * @brief   	Header file for main.c module.
+  * @date    	2015/11/30
+  * @brief   	Header file for adc.c.
   ******************************************************************************
   * @copy
   *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, ARMVietNam SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2009 ARMVietNam</center></h2>
-  */ 
+  */
   
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __ADC_H
