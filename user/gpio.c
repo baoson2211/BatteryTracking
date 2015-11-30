d92a43297bcb85729453d15f8eb1a1e87147fb4d
baoson2211 <baoson92@gmail.com>
2015-11-30 15:57:45 +0700

    Commit: 1. New functions: Set_() and Reset_IO() 2. Redefine LEDs order
    Signed-off-by: baoson2211 <baoson92@gmail.com>

--- user/gpio.c
+++ user/gpio.c
@@ -72,8 +72,8 @@ void GPIOInit(void) {
   GPIO_InitTypeDef GPIO_InitStructure; 
   EXTI_InitTypeDef  EXTI_InitStructure;
   
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | 
-                         RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE | RCC_APB2Periph_GPIOF , ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD | 
+                         RCC_APB2Periph_GPIOE | RCC_APB2Periph_GPIOF | RCC_APB2Periph_GPIOG, ENABLE);
   
   GPIO_StructInit(&GPIO_InitStructure); // Reset init structure, if not it can cause issues...
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_10 | GPIO_Pin_12 | GPIO_Pin_14 ;
@@ -111,12 +111,73 @@ void GPIOInit(void) {
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_Init(GPIOG, &GPIO_InitStructure);
   
+  GPIO_SetBits  ( GPIOB, GPIO_Pin_14);
   GPIO_ResetBits( GPIOB, GPIO_Pin_9 );
   GPIO_ResetBits( GPIOD, GPIO_Pin_8 );
   
   EXTI_InitStructure.EXTI_Line = EXTI_Line1;
   EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;  
+  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
   EXTI_InitStructure.EXTI_LineCmd = ENABLE;
   EXTI_Init(&EXTI_InitStructure);
+}
+
+void Set_IO(uint8_t ch){
+  switch (ch) {
+    case 0:  SET_LED01  ; break;
+    case 1:  SET_LED02  ; break;
+    case 2:  SET_LED03  ; break;
+    case 3:  SET_LED04  ; break;
+    case 4:  SET_LED05  ; break;
+    case 5:  SET_LED06  ; break;
+    case 6:  SET_LED07  ; break;
+    case 7:  SET_LED08  ; break;
+    case 8:  SET_LED09  ; break;
+    case 9:  SET_LED10  ; break;
+    case 10: SET_LED11  ; break;
+    case 11: SET_LED12  ; break;
+    case 12: SET_LED13  ; break;
+    case 13: SET_LED14  ; break;
+    case 14: SET_LED15  ; break;
+    case 15: SET_LED16  ; break;
+    case 16: SET_LED17  ; break;
+    case 17: SET_LED18  ; break;
+    case 18: SET_LED19  ; break;
+    case 19: SET_LED20  ; break;
+    case 20: SET_LED21  ; break;
+    case 21: SET_LED22  ; break;
+    case 22: SET_LED23  ; break; 
+    case 23: SET_LED24  ; break;
+    default:              break;
+  }
+}
+
+void Reset_IO(uint8_t ch){
+  switch (ch) {
+    case 0:  RESET_LED01  ; break;
+    case 1:  RESET_LED02  ; break;
+    case 2:  RESET_LED03  ; break;
+    case 3:  RESET_LED04  ; break;
+    case 4:  RESET_LED05  ; break;
+    case 5:  RESET_LED06  ; break;
+    case 6:  RESET_LED07  ; break;
+    case 7:  RESET_LED08  ; break;
+    case 8:  RESET_LED09  ; break;
+    case 9:  RESET_LED10  ; break;
+    case 10: RESET_LED11  ; break;
+    case 11: RESET_LED12  ; break;
+    case 12: RESET_LED13  ; break;
+    case 13: RESET_LED14  ; break;
+    case 14: RESET_LED15  ; break;
+    case 15: RESET_LED16  ; break;
+    case 16: RESET_LED17  ; break;
+    case 17: RESET_LED18  ; break;
+    case 18: RESET_LED19  ; break;
+    case 19: RESET_LED20  ; break;
+    case 20: RESET_LED21  ; break;
+    case 21: RESET_LED22  ; break;
+    case 22: RESET_LED23  ; break; 
+    case 23: RESET_LED24  ; break;
+    default:              break;
+  }
 }
--- user/gpio.h
+++ user/gpio.h
@@ -7,63 +7,63 @@
 *************************************************************************************************************************************
 */
 
-#define SET_LED01     { GPIO_SetBits(GPIOB, GPIO_Pin_15); }
-#define SET_LED02     { GPIO_SetBits(GPIOD, GPIO_Pin_9 ); }
-#define SET_LED03     { GPIO_SetBits(GPIOD, GPIO_Pin_11); }
-#define SET_LED04     { GPIO_SetBits(GPIOD, GPIO_Pin_13); }
-#define SET_LED05     { GPIO_SetBits(GPIOD, GPIO_Pin_15); }
-#define SET_LED06     { GPIO_SetBits(GPIOG, GPIO_Pin_3 ); }
-#define SET_LED07     { GPIO_SetBits(GPIOG, GPIO_Pin_5 ); }
-#define SET_LED08     { GPIO_SetBits(GPIOG, GPIO_Pin_7 ); }
-#define SET_LED09     { GPIO_SetBits(GPIOC, GPIO_Pin_6 ); }
-#define SET_LED10     { GPIO_SetBits(GPIOC, GPIO_Pin_8 ); }
-#define SET_LED11     { GPIO_SetBits(GPIOA, GPIO_Pin_8 ); }
-#define SET_LED12     { GPIO_SetBits(GPIOA, GPIO_Pin_10); }
-#define SET_LED13     { GPIO_SetBits(GPIOA, GPIO_Pin_12); }
-#define SET_LED14     { GPIO_SetBits(GPIOA, GPIO_Pin_14); }
-#define SET_LED15     { GPIO_SetBits(GPIOC, GPIO_Pin_10); }
-#define SET_LED16     { GPIO_SetBits(GPIOC, GPIO_Pin_12); }
-#define SET_LED17     { GPIO_SetBits(GPIOD, GPIO_Pin_1 ); }
-#define SET_LED18     { GPIO_SetBits(GPIOD, GPIO_Pin_3 ); }
-#define SET_LED19     { GPIO_SetBits(GPIOD, GPIO_Pin_5 ); }
-#define SET_LED20     { GPIO_SetBits(GPIOD, GPIO_Pin_7 ); }
-#define SET_LED21     { GPIO_SetBits(GPIOG, GPIO_Pin_10); }
-#define SET_LED22     { GPIO_SetBits(GPIOG, GPIO_Pin_12); }
-#define SET_LED23     { GPIO_SetBits(GPIOG, GPIO_Pin_14); }
-#define SET_LED24     { GPIO_SetBits(GPIOB, GPIO_Pin_3 ); }
+#define SET_LED01     { GPIO_SetBits(GPIOB, GPIO_Pin_15); }//1
+#define SET_LED13     { GPIO_SetBits(GPIOD, GPIO_Pin_9 ); }//2
+#define SET_LED02     { GPIO_SetBits(GPIOD, GPIO_Pin_11); }//3
+#define SET_LED14     { GPIO_SetBits(GPIOD, GPIO_Pin_13); }//4
+#define SET_LED03     { GPIO_SetBits(GPIOD, GPIO_Pin_15); }//5
+#define SET_LED15     { GPIO_SetBits(GPIOG, GPIO_Pin_3 ); }//6
+#define SET_LED04     { GPIO_SetBits(GPIOG, GPIO_Pin_5 ); }//7
+#define SET_LED16     { GPIO_SetBits(GPIOG, GPIO_Pin_7 ); }//8
+#define SET_LED05     { GPIO_SetBits(GPIOC, GPIO_Pin_6 ); }//9
+#define SET_LED17     { GPIO_SetBits(GPIOC, GPIO_Pin_8 ); }//10
+#define SET_LED06     { GPIO_SetBits(GPIOA, GPIO_Pin_8 ); }//11
+#define SET_LED18     { GPIO_SetBits(GPIOA, GPIO_Pin_10); }//12
+#define SET_LED07     { GPIO_SetBits(GPIOA, GPIO_Pin_12); }//13
+#define SET_LED19     { GPIO_SetBits(GPIOA, GPIO_Pin_14); }//14
+#define SET_LED08     { GPIO_SetBits(GPIOC, GPIO_Pin_10); }//15
+#define SET_LED20     { GPIO_SetBits(GPIOC, GPIO_Pin_12); }//16
+#define SET_LED09     { GPIO_SetBits(GPIOD, GPIO_Pin_1 ); }//17
+#define SET_LED21     { GPIO_SetBits(GPIOD, GPIO_Pin_3 ); }//18
+#define SET_LED10     { GPIO_SetBits(GPIOD, GPIO_Pin_5 ); }//19
+#define SET_LED22     { GPIO_SetBits(GPIOD, GPIO_Pin_7 ); }//20
+#define SET_LED11     { GPIO_SetBits(GPIOG, GPIO_Pin_10); }//21
+#define SET_LED23     { GPIO_SetBits(GPIOG, GPIO_Pin_12); }//22
+#define SET_LED12     { GPIO_SetBits(GPIOG, GPIO_Pin_14); }//23
+#define SET_LED24     { GPIO_SetBits(GPIOB, GPIO_Pin_3 ); }//24
 
-#define RESET_LED01   { GPIO_ResetBits(GPIOB, GPIO_Pin_15); }
-#define RESET_LED02   { GPIO_ResetBits(GPIOD, GPIO_Pin_9 ); }
-#define RESET_LED03   { GPIO_ResetBits(GPIOD, GPIO_Pin_11); }
-#define RESET_LED04   { GPIO_ResetBits(GPIOD, GPIO_Pin_13); }
-#define RESET_LED05   { GPIO_ResetBits(GPIOD, GPIO_Pin_15); }
-#define RESET_LED06   { GPIO_ResetBits(GPIOG, GPIO_Pin_3 ); }
-#define RESET_LED07   { GPIO_ResetBits(GPIOG, GPIO_Pin_5 ); }
-#define RESET_LED08   { GPIO_ResetBits(GPIOG, GPIO_Pin_7 ); }
-#define RESET_LED09   { GPIO_ResetBits(GPIOC, GPIO_Pin_6 ); }
-#define RESET_LED10   { GPIO_ResetBits(GPIOC, GPIO_Pin_8 ); }
-#define RESET_LED11   { GPIO_ResetBits(GPIOA, GPIO_Pin_8 ); }
-#define RESET_LED12   { GPIO_ResetBits(GPIOA, GPIO_Pin_10); }
-#define RESET_LED13   { GPIO_ResetBits(GPIOA, GPIO_Pin_12); }
-#define RESET_LED14   { GPIO_ResetBits(GPIOA, GPIO_Pin_14); }
-#define RESET_LED15   { GPIO_ResetBits(GPIOC, GPIO_Pin_10); }
-#define RESET_LED16   { GPIO_ResetBits(GPIOC, GPIO_Pin_12); }
-#define RESET_LED17   { GPIO_ResetBits(GPIOD, GPIO_Pin_1 ); }
-#define RESET_LED18   { GPIO_ResetBits(GPIOD, GPIO_Pin_3 ); }
-#define RESET_LED19   { GPIO_ResetBits(GPIOD, GPIO_Pin_5 ); }
-#define RESET_LED20   { GPIO_ResetBits(GPIOD, GPIO_Pin_7 ); }
-#define RESET_LED21   { GPIO_ResetBits(GPIOG, GPIO_Pin_10); }
-#define RESET_LED22   { GPIO_ResetBits(GPIOG, GPIO_Pin_12); }
-#define RESET_LED23   { GPIO_ResetBits(GPIOG, GPIO_Pin_14); }
-#define RESET_LED24   { GPIO_ResetBits(GPIOB, GPIO_Pin_3 ); }
+#define RESET_LED01   { GPIO_ResetBits(GPIOB, GPIO_Pin_15); }//1
+#define RESET_LED13   { GPIO_ResetBits(GPIOD, GPIO_Pin_9 ); }//2
+#define RESET_LED02   { GPIO_ResetBits(GPIOD, GPIO_Pin_11); }//3
+#define RESET_LED14   { GPIO_ResetBits(GPIOD, GPIO_Pin_13); }//4
+#define RESET_LED03   { GPIO_ResetBits(GPIOD, GPIO_Pin_15); }//5
+#define RESET_LED15   { GPIO_ResetBits(GPIOG, GPIO_Pin_3 ); }//6
+#define RESET_LED04   { GPIO_ResetBits(GPIOG, GPIO_Pin_5 ); }//7
+#define RESET_LED16   { GPIO_ResetBits(GPIOG, GPIO_Pin_7 ); }//8
+#define RESET_LED05   { GPIO_ResetBits(GPIOC, GPIO_Pin_6 ); }//9
+#define RESET_LED17   { GPIO_ResetBits(GPIOC, GPIO_Pin_8 ); }//10
+#define RESET_LED06   { GPIO_ResetBits(GPIOA, GPIO_Pin_8 ); }//11
+#define RESET_LED18   { GPIO_ResetBits(GPIOA, GPIO_Pin_10); }//12
+#define RESET_LED07   { GPIO_ResetBits(GPIOA, GPIO_Pin_12); }//13
+#define RESET_LED19   { GPIO_ResetBits(GPIOA, GPIO_Pin_14); }//14
+#define RESET_LED08   { GPIO_ResetBits(GPIOC, GPIO_Pin_10); }//15
+#define RESET_LED20   { GPIO_ResetBits(GPIOC, GPIO_Pin_12); }//16
+#define RESET_LED09   { GPIO_ResetBits(GPIOD, GPIO_Pin_1 ); }//17
+#define RESET_LED21   { GPIO_ResetBits(GPIOD, GPIO_Pin_3 ); }//18
+#define RESET_LED10   { GPIO_ResetBits(GPIOD, GPIO_Pin_5 ); }//19
+#define RESET_LED22   { GPIO_ResetBits(GPIOD, GPIO_Pin_7 ); }//20
+#define RESET_LED11   { GPIO_ResetBits(GPIOG, GPIO_Pin_10); }//21
+#define RESET_LED23   { GPIO_ResetBits(GPIOG, GPIO_Pin_12); }//22
+#define RESET_LED12   { GPIO_ResetBits(GPIOG, GPIO_Pin_14); }//23
+#define RESET_LED24   { GPIO_ResetBits(GPIOB, GPIO_Pin_3 ); }//24
 
 #define MUX00         { GPIO_ResetBits(GPIOB, GPIO_Pin_7 ); GPIO_ResetBits(GPIOB, GPIO_Pin_5 ); }
 #define MUX01         { GPIO_ResetBits(GPIOB, GPIO_Pin_7 ); GPIO_SetBits  (GPIOB, GPIO_Pin_5 ); }
 #define MUX10         { GPIO_SetBits  (GPIOB, GPIO_Pin_7 ); GPIO_ResetBits(GPIOB, GPIO_Pin_5 ); }
 #define MUX11         { GPIO_SetBits  (GPIOB, GPIO_Pin_7 ); GPIO_SetBits  (GPIOB, GPIO_Pin_5 ); }
 
-#define RELAY_FLIP    { GPIO_SetBits  (GPIOB, GPIO_Pin_7 ) ;}
-#define RELAY_FLOP    { GPIO_ResetBits(GPIOB, GPIO_Pin_7 ) ;}
+#define RELAY_FLIP    { GPIO_SetBits  (GPIOD, GPIO_Pin_8 ) ;}
+#define RELAY_FLOP    { GPIO_ResetBits(GPIOD, GPIO_Pin_8 ) ;}
 
 /*
 *************************************************************************************************************************************
@@ -77,3 +77,5 @@
 *************************************************************************************************************************************
 */
 void GPIOInit(void);
+void Set_IO  (uint8_t ch);
+void Reset_IO(uint8_t ch);
